0a5a4a115a9f1a3350b4a42dbde91b678e49dba6
raynerng <rayner_ng1997@live.com>
2024-04-09 20:45:44 +0800

    Add phototaking functionality

--- views/CameraScreen.jsx
+++ views/CameraScreen.jsx
@@ -1,17 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { View, Button } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect } from "react";
+import { View, Button } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
+
+  async function takePicture() {
+    if (camera) {
+      const options = { quality: 0.5 };
+      const data = await camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  }
 
   if (hasPermission === null) {
     return <View />;
@@ -22,8 +31,9 @@ export default function CameraScreen() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type}>
+      <Camera style={{ flex: 1 }} type={type} ref={(ref) => setCamera(ref)}>
         {/* Rest of your camera UI here */}
+        <Button title="Take picture" onPress={() => takePicture()} />
       </Camera>
       <Button
         title="Flip Camera"
