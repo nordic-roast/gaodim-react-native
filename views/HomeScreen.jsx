235e62e1a9326af6fd957a35990c687bdacb20c1
raynerng <rayner_ng1997@live.com>
2024-04-14 00:50:03 +0800

    Retrieve user info to display on home screen

--- views/HomeScreen.jsx
+++ views/HomeScreen.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
 
 import { auth } from "../firebaseConfig";
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 
 export default function HomeScreen({ navigation }) {
+  const [userEmail, setUserEmail] = useState("");
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUserEmail(user.email);
+    });
+  }, []);
+
   function handleLogout() {
     signOut(auth).then(() => {
       console.log("signout successful");
@@ -12,9 +19,36 @@ export default function HomeScreen({ navigation }) {
     });
   }
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Status of tickets + Money saved</Text>
-      <Button title="Logout" onPress={handleLogout} />
+    <View
+      style={{
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <View
+        style={{
+          height: "25%",
+          width: "50%",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          backgroundColor: "red",
+        }}
+      >
+        <Text>Welcome to GaoDim, {userEmail}</Text>
+        <Button title="Logout" onPress={handleLogout} />
+      </View>
+      <View
+        style={{
+          height: "25%",
+          width: "50%",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          backgroundColor: "green",
+        }}
+      >
+        <Text>Status of tickets + Money saved</Text>
+      </View>
     </View>
   );
 }
