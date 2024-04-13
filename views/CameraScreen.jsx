ab1d9e668853bc9e436591965a65e3c18b764078
raynerng <rayner_ng1997@live.com>
2024-04-14 00:49:12 +0800

    Transferred ticket creation logic to AppealScreen.jsx

--- views/CameraScreen.jsx
+++ views/CameraScreen.jsx
@@ -3,35 +3,15 @@ import { View, Button } from "react-native";
 import { Camera } from "expo-camera";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
-import { push, ref as databaseRef, set } from "firebase/database";
 import {
   ref as storageRef,
   uploadBytes,
   getDownloadURL,
 } from "firebase/storage";
-import { database, storage } from "../firebaseConfig";
-import OCRImage from "../vision";
+import { storage } from "../firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
 
-const DB_TICKETS_KEY = "tickets";
 const STORAGE_KEY = "images/";
-
-async function createTicket(url, navigation) {
-  const ticketListRef = databaseRef(database, DB_TICKETS_KEY);
-  const newTicketRef = push(ticketListRef);
-  const newDate = new Date();
-  console.log("url2 >>>>>>", url);
-  OCRImage(url).then((response) => {
-    console.log(response);
-    set(newTicketRef, {
-      date: newDate.toString(),
-      url: url,
-      parsedText: response,
-    });
-    console.log("ticket created!");
-  });
-  navigation.navigate("Appeal", { imageURL: url });
-}
 
 async function uploadImage(uri, navigation) {
   const key = uuidv4();
@@ -43,7 +23,7 @@ async function uploadImage(uri, navigation) {
       console.log("image uploaded!");
       getDownloadURL(fullStorageRef, key).then((url) => {
         console.log("url >>>>>", url);
-        createTicket(url, navigation);
+        navigation.navigate("Appeal", { imageURL: url });
       });
     })
     .catch((error) => {
